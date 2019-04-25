0e3f83f5ead47cee231d1c91a5130e1afdc0f206
patrick <playbydeone@gmail.com>
2019-04-25 18:12:32 +0100

    Mise à jour de lactivity SIGNUP commit

--- app/src/main/java/com/example/kofisa/Controllers/SignUpActivity.java
+++ app/src/main/java/com/example/kofisa/Controllers/SignUpActivity.java
@@ -2,10 +2,18 @@ package com.example.kofisa.Controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.example.kofisa.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private EditText name, email_id, passwordcheck;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "";
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
